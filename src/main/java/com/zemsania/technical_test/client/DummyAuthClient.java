55874df74cd08f016914bbe2c2443517fe43c273
Deivid30Medina <deivis32sebas02@gmail.com>
2025-06-18 23:08:08 -0500

    feat(api): Create DummyAuthClient with login and profile endpoints

---
+++ src/main/java/com/zemsania/technical_test/client/DummyAuthClient.java
@@ -0,0 +1,20 @@
+package com.zemsania.technical_test.client;
+
+import com.zemsania.technical_test.dto.AuthRequestDTO;
+import com.zemsania.technical_test.dto.AuthResponseDTO;
+import com.zemsania.technical_test.dto.UserInfoDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "dummyAuth", url = "${dummyjson.base-url}")
+public interface DummyAuthClient {
+
+    @PostMapping("/auth/login")
+    AuthResponseDTO login(@RequestBody AuthRequestDTO request);
+
+    @GetMapping("/auth/me")
+    UserInfoDTO getMe(@RequestHeader("Cookie") String cookie);
+}
